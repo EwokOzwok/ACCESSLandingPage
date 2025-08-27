5e887326feadf49faef1a3e69ea9668b2c8121bc
Ozmat <eozmat@albany.edu>
2025-08-27 11:30:57 -0400

    fixed favicon

--- R/app_ui.R
+++ R/app_ui.R
@@ -79,10 +79,10 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
-  # add_resource_path(
-  #   "www",
-  #   app_sys("app/www")
-  # )
+  add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
 
   tags$head(
     favicon(),
